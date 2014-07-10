716ed9bd542fa7d432b150578f482f4eea6f9c33
echicken <echicken@chickenbook.(none)>
2014-07-09 22:38:02 -0400

    Initial.

---
+++ test.js
@@ -0,0 +1,13 @@
+var ANSI = require('./ansi.js'),
+	fs = require('fs');
+
+var a = new ANSI();
+a.fromFile("./gnome.ans");
+fs.writeFileSync("gnome.txt", a.plainText, { 'encoding' : 'binary' });
+fs.writeFileSync(
+	"gnome.html",
+	"<html><body>" + a.HTML + "</body></html>",
+	{ 'encoding' : 'binary' }
+);
+fs.writeFileSync("gnome.bin", a.binary.data);
+console.log(a.binary.width);
