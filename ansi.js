b878a34c2d39b180a60503309a737aa309b09968
echicken <echicken@chickenbook.(none)>
2014-07-09 22:37:50 -0400

    Moved test stuff to new test.js, made ANSI into a module.

--- ansi.js
+++ ansi.js
@@ -305,9 +305,4 @@ var ANSI = function() {
 
 }
 
-var a = new ANSI();
-a.fromFile("./gnome.ans");
-fs.writeFileSync("gnome.txt", a.plainText, { 'encoding' : 'binary' });
-fs.writeFileSync("gnome.html", "<html><body>" + a.HTML + "</body></html>", { 'encoding' : 'binary' });
-fs.writeFileSync("gnome.bin", a.binary.data);
-console.log(a.binary.width);
+module.exports = ANSI;
